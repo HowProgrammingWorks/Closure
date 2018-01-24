0a962d631d678ba3183fd8ee71c56e5d258b0d11
DanDenisyuk <thunder.storm@ukr.net>
2018-01-24 13:41:56 +0200

    Mutable adder

---
+++ JavaScript/a-adder.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const adder = (a) => {
+  const obj = { value, add };
+  function value() {
+    return a;
+  }
+  function add(b) {
+    a += b;
+    return obj;
+  }
+  return obj;
+};
+
+const v = adder(3).add(-9).add(12).value();
+console.log(v);
