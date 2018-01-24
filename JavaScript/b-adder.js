69bd76ffa693844c9fbbb5030d91c35b1cabf287
tshemsedinov <timur.shemsedinov@gmail.com>
2018-01-24 20:50:59 +0200

    Optimized adder

---
+++ JavaScript/b-adder.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const adder = (a) => ({
+  value() {
+    return a;
+  },
+  add(b) {
+    a += b;
+    return this;
+  }
+});
+
+const v = adder(3).add(-9).add(12).value();
+console.log(v);
