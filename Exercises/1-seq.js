89a424a803c9ce89fb528977add38266743cad1e
ser <ser@localhost.localdomain>
2019-12-09 23:24:04 +0200

    Used recursion.

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const seq = (...f) => a => (typeof a === 'number' ?
-  f.reduce((v, fn) => fn(v), a) :
-  seq(a, ...f));
+const seq = fn => arg => {
+  if (typeof arg === 'number') return fn(arg);
+  return seq(x => fn(arg(x)));
+};
 
 module.exports = { seq };
