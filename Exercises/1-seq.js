4d2e1d07ec9e0397a785d8c8d3d8c9a4803abffd
alexandr.pavletsov <alexandr.pavletsov@rcntec.com>
2019-12-10 22:43:53 +0500

    one line

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const seq = f => g => x => ((typeof x === 'number') ?
-  f(g(x)) :
-  seq(i => f(g(i)))(x));
+const seq = f => g => x => (typeof x === 'number' ? f(g(x)) : seq(i => f(g(i)))(x));
 
 module.exports = { seq };
