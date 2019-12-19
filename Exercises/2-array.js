7248285e78ebd65ab352e5005c832c6a2e9520ec
Daniil Denisyuk <ordekonec@gmail.com>
2019-12-19 08:08:50 +0200

    tasks

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -8,19 +8,4 @@ const array = () => {
   return f;
 };
 
-
-const arr = array();
-
-arr.push('first');
-arr.push('second');
-arr.push('third');
-console.log(arr(0)); // Выводит: first
-console.log(arr(1)); // Выводит: second
-console.log(arr(2)); // Выводит: third
-console.log(arr.pop()); // Выводит: third
-console.log(arr.pop()); // Выводит: second
-console.log(arr.pop()); // Выводит: first
-
-console.log(arr.pop());
-
 module.exports = { array };
