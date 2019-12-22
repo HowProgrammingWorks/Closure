ea5f600de859f090c9be5483d68193e2f5a74da2
Ovsky Aleksander <masterguyver@gmail.com>
2019-12-22 13:24:33 +0200

    Tasks compleeted. Question. Is not wrong my way of seq?

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const seq = f => g => x => 0;
-
+const seq = f => g => x => (typeof x === 'number' ?
+  (typeof f === 'function' ? f(g(x)) : g(x)) : seq(f(g(x))));
 module.exports = { seq };
--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,26 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const a = [];
+  const m = i => a[i];
+  const push = obj => a.push(obj);
+  const pop = () => a.pop();
+  Object.assign(m, { push, pop });
+  return m;
+};
+
+const arr = array();
+
+arr.push('first');
+arr.push('second');
+arr.push('third');
+console.log(arr(0)); 
+console.log(arr(1)); 
+console.log(arr(2)); 
+console.log(arr.pop()); 
+console.log(arr.pop()); 
+console.log(arr.pop()); 
+
+console.log(arr.pop());
 
 module.exports = { array };
