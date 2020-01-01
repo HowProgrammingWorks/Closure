5e5ec1cd21b273e0c70ba84a4ac474b6b9c82e23
Viktor.Orlyk <victor.orlyk93@gmail.com>
2020-01-01 15:14:46 +0200

    Revert "all tasks are done."

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  const fn = i => arr[i];
+  fn.push = x => arr.push(x);
+  fn.pop = () => arr.pop();
+  return fn;
+};
 
 module.exports = { array };
