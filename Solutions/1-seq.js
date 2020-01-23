3b4d90e0b15575eccc2a33e2dedc448febad204f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-01-24 01:50:52 +0200

    Add solutions

---
+++ Solutions/1-seq.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const seq = f => g => (typeof g === 'number' ? f(g) : seq(x => f(g(x))));
+
+module.exports = { seq };
---
+++ Solutions/2-array.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const array = () => {
+  const data = [];
+  const get = i => data[i];
+  get.push = x => data.push(x);
+  get.pop = () => data.pop();
+  return get;
+};
+
+module.exports = { array };
