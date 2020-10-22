a51375b3b953dae4ed8380807987f687146b2a1f
dopolZ <dopolz@gmail.com>
2020-10-22 11:33:19 +0500

    ok

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const a = [];
+  const b = c => a[c];
+  b.push = d => (a.push(d));
+  b.pop = () => a.pop();
+  return b;
+};
 
 module.exports = { array };
