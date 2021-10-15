a1e0b4c6b3387a2f109313aa0b47d22614b27db6
Grevcev.AS <grevcev.as@dns-shop.ru>
2021-10-15 18:46:34 +1000

    lab closures

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  const search = i => arr[i];
+  search.push = (v) => arr.push(v);
+  search.pop = () => arr.pop();
+
+  return search;
+};
 
 module.exports = { array };
