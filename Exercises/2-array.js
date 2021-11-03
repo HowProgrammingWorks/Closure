9ee66feccf1956099f20f13459e7e99ee3c8f947
vladimirvikulin <v.vikulin.im11@kpi.ua>
2021-11-03 21:29:18 +0200

    Closure

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,17 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  const get = (i) => arr[i];
+  get.push = (x) => arr.push(x);
+  get.pop = () => arr.pop();
+  return get;
+};
+
+const a = array();
+a.push('first');
+console.log(a(0));
+console.log(a.pop());
+console.log(a.pop());
 
 module.exports = { array };
