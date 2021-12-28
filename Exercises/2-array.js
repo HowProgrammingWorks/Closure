f7dba81460332f4a8a8fa89cc41da9966fbbc9be
AIDXYLELELE <warboyka48@gmail.com>
2021-12-28 20:55:08 +0200

    Lab_by_Aid

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+    const value = [];
+    const a = (index) => value[index];
+    a.push = (x) => value.push(x);
+    a.pop = () => value.pop();
+    return a;
+};
 
 module.exports = { array };
