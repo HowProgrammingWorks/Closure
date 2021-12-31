4005574bb2ea3792dea248adb4c13c22dc96cd38
Orbis <infectores@gmail.com>
2021-12-31 13:25:25 +0300

    homework

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const seq = (f) => (g) => (x) => 0;
+const seq = (f) => (n) => (typeof n === 'number' ? f(n) : seq((a) => f(n(a))));
 
 module.exports = { seq };
--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  const f = (a) => arr[a];
+  f['push'] = (a) => arr.push(a);
+  f['pop'] = (a) => arr.pop(a);
+  return f;
+};
 
 module.exports = { array };
