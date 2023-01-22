b38d8e1569e50ac079c1cce7b174e6aaa35ee1ff
Tima Gixe <timagixe@protonmail.com>
2023-01-22 20:31:54 +0200

    Solves 2-array

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  const callback = (index) => arr[index];
+  callback.push = (value) => arr.push(value);
+  callback.pop = () => arr.pop();
+  return callback;
+};
 
 module.exports = { array };
