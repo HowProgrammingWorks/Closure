a511d9534a9c98f8fe3e17da062972b2949e6f31
Jhon Mosk <jhon-mosk@yandex.ru>
2023-07-14 14:49:45 +0300

    Lab work is ready

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const data = [];
+  const obj = (index) => data[index];
+
+  obj.push = (value) => data.push(value);
+  obj.pop = () => data.pop();
+
+  return obj;
+};
 
 module.exports = { array };
--- JavaScript/2-chain.js
+++ JavaScript/2-chain.js
@@ -1,22 +1,17 @@
-'use strict';
+"use strict";
 
 const hash = () => {
   const data = {};
-  Object.defineProperty(data, 'add', {
+  Object.defineProperty(data, "add", {
     enumerable: false,
     value(key, value) {
       data[key] = value;
       return data;
-    }
+    },
   });
   return data;
 };
 
 // Usage
 
-console.dir(
-  hash()
-    .add('name', 'Marcus')
-    .add('city', 'Roma')
-    .add('born', 121)
-);
+console.dir(hash().add("name", "Marcus").add("city", "Roma").add("born", 121));
