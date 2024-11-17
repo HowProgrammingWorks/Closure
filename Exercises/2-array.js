1236a873daf31e2a39869ed5705a14ccbb7999b4
Viktor Agapov <agapov.v@domohotels.ru>
2024-11-17 20:30:31 +0700

    Exercises

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const data = [];
+  const get = (i) => data[i];
+  get.push = (x) => data.push(x);
+  get.pop = () => data.pop();
+  return get;
+};
 
 module.exports = { array };
